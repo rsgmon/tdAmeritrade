43df1e0112be4f510dcb4e39ecb7381c838e965a
Ryeland <rsgmon@gmail.com>
2018-09-13 09:15:43 -0700

    updated readme

--- README.md
+++ README.md
@@ -45,15 +45,114 @@ _authServer.py_ is really just a duplicate from td's developer site. I left it i
 
 Go to [Simple Auth for Local Apps](https://developer.tdameritrade.com/content/simple-auth-local-apps) to find out how to get a key.
 
+This is the absolute simplest call. It will return a quote for SPY.
 ```
 from tdAmeritrade import tdCall
 td_call = tdCall.tdCall([your access key])
 print(td_call.get_td_data())
 ```
 
+From there you can make more interesting requests. The below will get weekly price history for the last 3 months and options data for Apple and Netflix.  
+```
+from tdAmeritrade import tdCall
+td_call = tdCall.tdCall([your access key], symbols=["AAPL","NFLX"], config=[{"route": "h", "periodType": "month", "period": 3, "frequencyType": "daily"}, {"route";"o"})
+print(td_call.get_td_data())
+```
+
+
 ## Contributions
 Accepting all contributions! You may notice that not all of TDAmeritrade's API routes are included. Well that's because we haven't had to use them so far. Build or update a route!
 
 Or often the data comes in a messy nested glob. We have helper functions that handle the data once received and we want even add complete reports. Build one!
 
+## Reference
+Help on module tdCall:
+
+NAME
+    tdCall
+
+CLASSES
+    builtins.object
+        tdCall
+
+    class tdCall(builtins.object)
+       Prepares arguments for making HTTP calls to TDAmeritrade API.
+       Gets data from TDAmeritrade API.
+       Processes the result from TDAmeritrade.
+     
+       Methods defined here:
+     
+       __init__(self, key, symbols='SPY', configs=None)
+           :param key: string TDAmeritrade Access Key
+           :param symbols: string or list, Stock Symbols
+           :param configs: dict or list of dicts,
+               required:
+                   route: string
+                       "q" : qoutes endpoint
+                       "h" : price history endpoint
+                       "o" : options endpoint
+               optional:
+                   Each endpoint has different parameters. See TDAmeritrade's developer website.
+     
+               Examples:
+                   This will get daily historical prices for the last three months.
+                   {"route": "h", "periodType": "month", "period": 3, "frequencyType": "daily"}
+     
+                   This will get vertical spreads for just calls, within one strike from at the money.
+                   {"route": "o", "contractType": "CALL", "strikeCount": 3, "strategy": "VERTICAL"}
+     
+                   This will get quotes and options with all other arguments using the api's default.
+                   [{"route": "q"}, {"route": "o"}]
+     
+       flatten_response(self, list_of_lists_of_prices)
+           Flattens the response to a single list of dicts.
+           :param list_of_lists_of_dicts:
+           :return: list_of_dicts
+     
+       get_configs(self)
+           :return: prepared config object capable of completely setting up HTTP get arguments
+     
+       get_coroutines(self, configs, symbols)
+            Using the ClientSession it creates a list of functions ready to be executed. Then using gather asynchronous calls are made using the list.
+           :param configs: list
+           :param symbols: list
+           :return: coroutine functions
+       get_symbol_lists(self, symbol_file_name)
+           Given a file name will asynchronously parse a list of symbols into a list of lists of symbols with each member list being 10 symbols long. This function was created to allow making a get request for the 10 symbols, processing the results (for example saving locally), an then making another get request. This assists in the event there is an error in getting data, the entire list need not be retreived.
+           :param symbol_file_name:
+           :return: list of lists of symbols
+     
+       get_td_data(self)
+           Wrapper to get TDAmeritrade Data
+           :return: list
+     
+       historical_prices(self, session, symbol, get_params)
+           :param session: obj
+           :param symbol: list
+           :param get_params: dict
+           :return: func
+     
+       multi_quote(self, session, symbols, get_params)
+           :param session: obj
+           :param symbol: list
+           :param get_params: dict
+           :return: func
+     
+       options(self, session, get_params)
+           :param session: obj
+           :param get_params: dict
+           :return: func
+     
+       set_parse_symbols(self, symbol_file_name)
+           :param symbol_file_name: string
+           :return: list of list
+     
+       set_single_config(self, raw_config)
+           :param raw_config: dict The user provided configuration.
+           :return: a single config object
+     
+       wrap_schedule(self, coroutines)
+           Scheduler for making asynchronous calls.
+           :param coroutines:
+           :return: list_of_lists_dict
 
--- tdAmeritrade/tdCall.py
+++ tdAmeritrade/tdCall.py
@@ -4,8 +4,35 @@ import async_timeout
 import copy
 
 class tdCall(object):
+    """
+    Prepares arguments for making HTTP calls to TDAmeritrade API.
+    Gets data from TDAmeritrade API.
+    Processes the result from TDAmeritrade.
+    """
     base_url = "https://api.tdameritrade.com/v1/"
     def __init__(self, key, symbols="SPY", configs=None):
+        """
+        :param key: string TDAmeritrade Access Key
+        :param symbols: string or list, Stock Symbols
+        :param configs: dict or list of dicts,
+            required:
+                route: string
+                    "q" : qoutes endpoint
+                    "h" : price history endpoint
+                    "o" : options endpoint
+            optional:
+                Each endpoint has different parameters. See TDAmeritrade's developer website.
+
+            Examples:
+                This will get daily historical prices for the last three months.
+                {"route": "h", "periodType": "month", "period": 3, "frequencyType": "daily"}
+
+                This will get vertical spreads for just calls, within one strike from at the money.
+                {"route": "o", "contractType": "CALL", "strikeCount": 3, "strategy": "VERTICAL"}
+
+                This will get quotes and options with all other arguments using the api's default.
+                [{"route": "q"}, {"route": "o"}]
+        """
         self.key = key
         self.raw_configs = configs
         if isinstance(symbols, str):
@@ -13,7 +40,10 @@ class tdCall(object):
         else: self.symbols = symbols
 
     def get_configs(self):
-        """test"""
+        """
+
+        :return: prepared config object capable of completely setting up HTTP get arguments
+        """
         if not self.raw_configs:
             return [self.quotes]
         else:
@@ -26,6 +56,11 @@ class tdCall(object):
                 return refined_configs
 
     def set_single_config(self, raw_config):
+        """
+
+        :param raw_config: dict The user provided configuration.
+        :return: a single config object
+        """
         if not raw_config:
             raise ValueError("Your configuration was empty. You don't have to include a configuration but if you do it must contain values.")
         elif 'route' not in raw_config.keys():
@@ -42,12 +77,21 @@ class tdCall(object):
         return static_config
 
     def get_symbol_lists(self, symbol_file_name):
+        """
+        Given a file name will asynchronously parse a list of symbols into a list of lists of symbols with each member list being 10 symbols long. This function was created to allow making a get request for the 10 symbols, processing the results (for example saving locally), an then making another get request. This assists in the event there is an error in getting data, the entire list need not be retreived.
+        :param symbol_file_name:
+        :return: list of lists of symbols
+        """
         coroutine = self.set_parse_symbols(symbol_file_name)
         symbols = self.wrap_schedule(coroutine)
         return symbols
 
     async def get_coroutines(self, configs, symbols):
-        """ Accepts a list of objects each containing http get pars. Using the ClientSession it creates a list of functions ready to be executed. Then using gather asynchronous calls are made using the list.
+        """
+         Using the ClientSession it creates a list of functions ready to be executed. Then using gather asynchronous calls are made using the list.
+        :param configs: list
+        :param symbols: list
+        :return: coroutine functions
         """
         async with aiohttp.ClientSession() as session:
             all_calls = []
@@ -70,6 +114,12 @@ class tdCall(object):
             return all_calls
 
     async def historical_prices(self, session, symbol, get_params):
+        """
+        :param session: obj
+        :param symbol: list
+        :param get_params: dict
+        :return: func
+        """
         """Only works as part of the the get_data function. It essentially sets everything up for an http call."""
         with async_timeout.timeout(100):
             async with session.get(self.base_url + get_params['path'][0] + "/" + symbol + "/" + get_params['path'][1],
@@ -77,35 +127,64 @@ class tdCall(object):
                 return await response.json()
 
     async def options(self, session, get_params):
-        """Only works as part of the the get_data function. It essentially sets everything up for an http call."""
+        """
+        :param session: obj
+        :param get_params: dict
+        :return: func
+        """
         with async_timeout.timeout(100):
             async with session.get(self.base_url + get_params['path'][0] + "/" + get_params['path'][1],
                                    headers=get_params['headers'], params=get_params['param_vars']) as response:
                 return await response.json()
 
     async def multi_quote(self, session, symbols, get_params):
+        """
+        :param session: obj
+        :param symbol: list
+        :param get_params: dict
+        :return: func
+        """
         with async_timeout.timeout(100):
             async with session.get(self.base_url + get_params['path'][0] + "/" + get_params['path'][1],
                                    headers=get_params['headers'], params={'symbol': symbols}) as response:
                 return await response.json()
 
     def get_td_data(self):
+        """
+        Wrapper to get TDAmeritrade Data
+        :return: list
+        """
         configurations = self.get_configs()
         coroutines = self.get_coroutines(configurations, self.symbols)
         return self.wrap_schedule(coroutines)
 
     def flatten_response(self, list_of_lists_of_prices):
+        """
+        Flattens the response to a single list of dicts.
+        :param list_of_lists_of_dicts:
+        :return: list_of_dicts
+        """
         result = []
         for list_of_list_of_prices in list_of_lists_of_prices:
             [result.append(p) for p in list_of_list_of_prices]
         return result
 
     def wrap_schedule(self, coroutines):
+        """
+        Scheduler for making asynchronous calls.
+        :param coroutines:
+        :return: list_of_lists_dict
+        """
         loop = asyncio.get_event_loop()
         task = asyncio.ensure_future(coroutines)
         return loop.run_until_complete(task)
 
     async def set_parse_symbols(self, symbol_file_name):
+        """
+
+        :param symbol_file_name: string
+        :return: list of list
+        """
         with open(symbol_file_name, 'r') as file:
             symbols = []
             for l in file:
@@ -120,6 +199,11 @@ class tdCall(object):
         return symbols
 
     async def _parse_line(self, line):
+        """
+
+        :param line:
+        :return: list
+        """
         parsed_line = "[" + line[:-1] + "]"
         if len(parsed_line) > 10:
             queue = [parsed_line[i:i + 10] for i in range(0, len(parsed_line), 10)]
@@ -165,8 +249,6 @@ class tdCall(object):
     def quotes(self):
         self._quotes['get_params']['headers']['Authorization'] = "Bearer " + self.key
         return self._quotes
-
-
 
     _options = {
         "func": "options",
